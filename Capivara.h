fed540162831a293558954fd7463a163e8f27de6
Samuel Nascimento <safn@cin.ufpe.br>
2026-05-26 13:07:47 -0300

    fixing problems

--- Capivara.h
+++ Capivara.h
@@ -2,25 +2,14 @@
 #define CAPIVARA_H_
 
 #include "Inimigo.h"
-<<<<<<< HEAD
-=======
 #include <vector>
 #include "Projetil.h"
->>>>>>> ceac183ef39e7b197a8c30da83078980193d55da
 
 class Capivara : public Inimigo {
 
 public:
-<<<<<<< HEAD
     Capivara(int pos_x, int pos_y, int vida = 1) : Inimigo(pos_x, pos_y, false, vida) { std::cout << "Objeto do tipo Capivara CRIADO!" << endl; }
     ~Capivara() override {}
-=======
-    Capivara(float pos_x, float pos_y, int vida, std::vector<Projetil*>& tiros);
-    
-    ~Capivara() override;
-
-    void update(int player_x, int player_y) override;
->>>>>>> ceac183ef39e7b197a8c30da83078980193d55da
 };
 
 #endif
--- Tucano.h
+++ Tucano.h
@@ -2,12 +2,8 @@
 #define _TUCANO_H_
 
 #include "Inimigo.h"
-<<<<<<< HEAD
-#include <iostream>
-=======
 #include <vector>
 #include "Projetil.h"
->>>>>>> ceac183ef39e7b197a8c30da83078980193d55da
 
 class Tucano : public Inimigo {
 
@@ -15,15 +11,12 @@ public:
     Tucano(int pos_x, int pos_y, int vida = 1) : Inimigo(pos_x, pos_y, true, vida) { std::cout << "Objeto do tipo Tucano CRIADO!" << endl; }
     ~Tucano() override {}
 
-<<<<<<< HEAD
-=======
     float getVelocidade();
     void setVelocidade(float nova_vel);
     int getDirection();
     void setDirection(int dir);
 
     void update(int player_x, int player_y) override;
->>>>>>> ceac183ef39e7b197a8c30da83078980193d55da
 };
 
 #endif
